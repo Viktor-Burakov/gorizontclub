8530386ac6ba29244f8bd4c204576c98cfac7f3f
viktor.burakov <burakov.tmn@gmail.com>
2023-02-04 11:21:10 +0500

    take out services

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers\Post;
 
-<<<<<<< HEAD
-=======
-use App\Models\Posts;
->>>>>>> aad975b85fbfa9bffa1156b242975552069842d4
 
 
 class IndexController extends BaseController
--- app/Http/Controllers/Post/ShowController.php
+++ app/Http/Controllers/Post/ShowController.php
@@ -2,12 +2,6 @@
 
 namespace App\Http\Controllers\Post;
 
-<<<<<<< HEAD
-=======
-use App\Models\Posts;
-
-
->>>>>>> aad975b85fbfa9bffa1156b242975552069842d4
 class ShowController extends BaseController
 {
    public function __invoke($uri)
